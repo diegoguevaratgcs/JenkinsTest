f9ee7c6dfe02eb81fa200e4eb52577b3822611dc
diegoguevaratgcs <48328623+diegoguevaratgcs@users.noreply.github.com>
2019-03-07 11:44:49 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,19 +15,19 @@ int main(int argc, char** argv)
         // Prints the first two terms.
         if(i == 1)
         {
-            cout << " " << t1;
+            cout << " " << t1<<endl;
             continue;
         }
         if(i == 2)
         {
-            cout << t2 << " ";
+            cout << t2 << " "<<endl;
             continue;
         }
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
         
-        cout << nextTerm << " ";
+        cout << nextTerm << " "<<endl;
     }
     return 0;
 }
