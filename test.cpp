fb0e617ff7ffb5d9dc31290efda436bf40caa0ff
diegoguevaratgcs <48328623+diegoguevaratgcs@users.noreply.github.com>
2019-03-07 11:25:09 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,45 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-int leedato()
+
+int main(int argc, char** argv)
 {
-    int n;
-cin>>n;
-return n;
-}
-int fibonacci(int n,int a,int b,int i)
-{
-    int c;
- 
-    c=a+b;
-    if(c<=n)
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    n = atoi(argv[1]);
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i)
     {
- 
-    cout<<c<<endl;
-    a=b; b=c;
-        return fibonacci(n,a,b,i+1);
+        // Prints the first two terms.
+        if(i == 1)
+        {
+            cout << " " << t1;
+            continue;
+        }
+        if(i == 2)
+        {
+            cout << t2 << " ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << " ";
     }
-    else
-        return 0;
-}
-void pideCantidad()
-{
- 
-    int n,i=0,a=1,b=1;
-    do
-    {
-    system("cls");
-    cout<<"Ingrese la cantidad de cadena para presentar: ";
-    n=leedato();
-    }while(n<3);
-    cout<<a<<endl;cout<<b<<endl;
-    fibonacci(n,a,b,i);
- 
-}
- 
-int main()
-{
- 
-    pideCantidad();
- 
+    return 0;
 }
